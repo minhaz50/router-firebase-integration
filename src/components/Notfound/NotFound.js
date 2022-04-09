6b99000da54f9f837aa93bcfc8782081517da965
minhaz2597 <md.minhazuddin2597@gmail.com>
2022-04-09 14:18:08 +0600

    first commit

---
+++ src/components/Notfound/NotFound.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./NotFound.css";
+
+const NotFound = () => {
+  return (
+    <div className="not-found-container">
+      <img
+        src="https://cdn.pixabay.com/photo/2019/12/27/08/36/page-not-found-4721931_960_720.png"
+        alt=""
+      />
+    </div>
+  );
+};
+
+export default NotFound;
---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA9IWJh3wXk4k_9ZYQvIPqVh7AbzdiHgyQ",
+  authDomain: "router-firebase-integrat-3197d.firebaseapp.com",
+  projectId: "router-firebase-integrat-3197d",
+  storageBucket: "router-firebase-integrat-3197d.appspot.com",
+  messagingSenderId: "296358467673",
+  appId: "1:296358467673:web:140952b5bc0837cc77b948",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
